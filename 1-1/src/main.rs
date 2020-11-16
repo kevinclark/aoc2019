d6faeb9794bc4e105456c02a2e6b8865d3c2f2fb
Kevin Clark <kevin.clark@gmail.com>
2020-11-16 08:20:00 -0800

    Simplify

--- 1-1/src/main.rs
+++ 1-1/src/main.rs
@@ -6,22 +6,19 @@ use snafu::{ResultExt, Snafu};
 
 #[derive(Debug, Snafu)]
 pub enum Error {
-    UnableToOpenFile { source: io::Error },
-    UnableToReadLine { source: io::Error },
-    UnableToParseMass { source: std::num::ParseIntError },
+    OpenFile { source: io::Error },
+    ReadLine { source: io::Error },
+    ParseMass { source: std::num::ParseIntError },
 }
 
 fn main() -> Result<(), Error> {
-    let file = File::open("input.txt").context(UnableToOpenFile)?;
+    let file = File::open("input.txt").context(OpenFile)?;
     let reader = io::BufReader::new(file);
 
     let mut acc = 0;
 
     for line in reader.lines() {
-        let i: u32 = line
-            .context(UnableToReadLine)?
-            .parse()
-            .context(UnableToParseMass)?;
+        let i: u32 = line.context(ReadLine)?.parse().context(ParseMass)?;
 
         acc += (i / 3) - 2;
     }
