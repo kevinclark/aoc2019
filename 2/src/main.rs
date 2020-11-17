77e4ac6dbb5c68319eccf854c66bf18e86291194
Kevin Clark <kevin.clark@gmail.com>
2020-11-16 16:30:36 -0800

    Day 2 part 2

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -3,11 +3,23 @@ use std::fs;
 use day2::*;
 
 fn main() {
-    let mut mem = load_program(&fs::read_to_string("input.txt").unwrap());
-    // [B]efore running the program, replace position 1 with the value 12
-    // and replace position 2 with the value 2
-    mem[1] = 12;
-    mem[2] = 2;
-    execute(&mut mem);
-    println!("{:?}", mem[0]);
+    let initial_state = load_program(&fs::read_to_string("input.txt").unwrap());
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            let mut mem = initial_state.to_vec();
+            // [B]efore running the program, replace position 1 with the value 12
+            // and replace position 2 with the value 2
+            mem[1] = noun;
+            mem[2] = verb;
+            execute(&mut mem);
+
+            if mem[0] == 19690720 {
+                println!("Noun: {} Verb: {}", noun, verb);
+                println!("Answer: {}", (100 * noun) + verb);
+                return;
+            }
+        }
+    }
+
+    println!("Not found");
 }
