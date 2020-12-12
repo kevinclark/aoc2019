cc04250cee16f6fccd9b08776da25815644e9a77
Kevin Clark <kevin.clark@gmail.com>
2020-12-11 20:55:58 -0800

    Remove stale comment from day 2
    Was from part 1

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -7,18 +7,19 @@ fn main() {
         .arg(Arg::with_name("INPUT").required(true))
         .get_matches();
 
-    let input = &fs::read_to_string(matches.value_of("INPUT").unwrap()).unwrap();
+    let input =
+        &fs::read_to_string(matches.value_of("INPUT").unwrap()).unwrap();
 
     let initial_state = intcode::load_program(input);
 
     for noun in 0..=99 {
         for verb in 0..=99 {
             let mut mem = initial_state.to_vec();
-            // [B]efore running the program, replace position 1 with the value 12
-            // and replace position 2 with the value 2
+            let inputs: [i64; 0] = [];
+
             mem[1] = noun;
             mem[2] = verb;
-            let inputs: [i64; 0] = [];
+
             intcode::execute(&mut mem, &mut inputs.iter());
 
             if mem[0] == 19690720 {
